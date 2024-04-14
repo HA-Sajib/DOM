0500c23f340f935e22797fe893f5f869e9b8c1fc
HA-Sajib <mesajib2020@gmail.com>
2024-04-14 09:22:10 +0600

    DOM

---
+++ script.js
@@ -0,0 +1,50 @@
+// const mainHeading = document.getElementById("main-heading")\
+// const mainheading = document.querySelector("#main-heading")
+// const headline = document.querySelector(".headline")
+// const btn = document.querySelector(".btn")
+// const navitem = document.querySelectorAll()
+// console.log(btn)
+// console.log(mainheading)
+// console.log(headline)
+
+//------------- textContent and innerText দিয়ে html এর ভিতরের  content change  করা হয়
+
+// const mainheading = document.getElementById('main-heading')
+// console.log(mainheading.textContent)
+// mainheading.textContent = "change by js DOM"
+
+// const btnHeadline = document.querySelector(".btn-headline")
+// btnHeadline.textContent = "changed by dom"
+
+// const todoHeading = document.querySelector(".todo-heading")
+// todoHeading.textContent = "changed by DOM"
+
+// ------------------- styleing -----------------------------------------
+
+// const mainHeading = document.querySelector("div.headline h2") // এভাবেও select করা যায়। div er ভিতর headline নামের class এর মধ্যে h2 কে ধরা হলো।
+// console.log(mainHeading)
+
+// mainHeading.style.color = "red"
+
+// const btn = document.querySelector("div.headline button")
+// console.log(btn)
+// btn.style.color = "blue"
+// btn.style.background = "red"
+
+// const todoHeading = document.querySelector(".todo-heading")
+// console.log(todoHeading)
+// todoHeading.style.color = "red"
+// todoHeading.style.fontSize = "30px"
+
+// ------------------ get and set atteubutes--------------------------------
+
+// const navitems = document.getElementsByTagName("a")
+// // console.log(navitems)
+
+// for(i=0; i<navitems.length; i++){
+//     const navitem = navitems[i]
+//     navitem.style.background = "red"
+//     navitem.style.color = "blue"
+//     navitem.style.fontWeight = "bold"
+//     navitem.style.padding = "10px"
+// }
